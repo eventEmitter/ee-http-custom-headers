71906e037c6e1bd86801a498a9d7f1ddc80d6e1c
Michael Rüfenacht <ruefe@rcky.ch>
2013-12-02 18:59:07 +0100

    Cleanup

--- index.js
+++ index.js
@@ -1,9 +1,45 @@
+/**
+ * @todo Add a possiblity to directly hook in transformations
+ * @todo Add dedicated collections for select, order and filter statements
+ * @todo Allow to hook in additional rules for node transformation
+ */
+
 var parser      = require('./lib/parser/HeaderParser.js'),
     log         = require('ee-log'),
-    Class       = require('ee-class'),
-    NodeCollection = require('./lib/nodes/NodeCollection'),
-    PrettyPrinter = require('./example/PrettyPrinter');
+    Class       = require('ee-class');
 
-var pri = new PrettyPrinter();
-var result = parser.parse('user.mail!=null, user.birthday<diff(2013-11-10, NOW())', 'filter');
-console.log(pri.prettyPrint(result).join('\t AND \n'));
+/**
+ * The parser itself.
+ * @type {*|exports}
+ */
+module.exports.parser = parser;
+
+/**
+ * Parses a select statement into a collection of properties and variables.
+ *
+ * @param aString
+ * @returns {*}
+ */
+module.exports.parseSelect = function(aString){
+    return parser.parse(aString, 'select');
+};
+
+/**
+ * Parses a filter statement and creates a collection of comparison objects.
+ *
+ * @param aString
+ * @returns {*}
+ */
+module.exports.parseFilter = function(aString){
+    return parser.parse(aString, 'filter');
+};
+
+/**
+ * Parses an order statement and creates a collection of possibly tagged variable or property nodes.
+ *
+ * @param aString
+ * @returns {*}
+ */
+module.exports.parseOrder = function(aString){
+    return parser.parse(aString, 'order');
+};
