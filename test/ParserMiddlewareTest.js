77848e3191520e9b4c30a1293e5b32b8663ca6a7
mruefenacht <ruefe@rcky.ch>
2014-05-19 11:52:35 +0200

    Removed request propagation on parsing error

--- test/ParserMiddlewareTest.js
+++ test/ParserMiddlewareTest.js
@@ -18,20 +18,64 @@ var mockRequest         = {
     }
 };
 
-var mockParser          = {
-    counter: 0
-    , parse: function(value, rule){
+var erroneousFilterMockRequest = {
+    _headers: {
+        'accept': 'Application/JSON'
+        , 'filter': 'deleted == null, venues.address.postalcode<4500, venues.address.postalcode>4500'
+        , 'select': 'id > 100, title, description'
+        , 'order':  'id ASC, date DESC'
+    }
+
+    , getHeader: function(key, parse){
+        return this._headers[key] || null;
+    }
+};
+
+var erroneousSelectMockRequest = {
+    _headers: {
+        'accept': 'Application/JSON'
+        , 'filter': 'deleted = null, venues.address.postalcode<4500, venues.address.postalcode>4500'
+        , 'select': 'id >< 100, title, description'
+        , 'order':  'id ASC, date DESC'
+    }
+
+    , getHeader: function(key, parse){
+        return this._headers[key] || null;
+    }
+};
+
+var erroneousOrderMockRequest = {
+    _headers: {
+        'accept': 'Application/JSON'
+        , 'filter': 'deleted == null, venues.address.postalcode<4500, venues.address.postalcode>4500'
+        , 'select': 'id > 100, title, description'
+        , 'order':  'id == ASC, date DESC'
+    }
+
+    , getHeader: function(key, parse){
+        return this._headers[key] || null;
+    }
+};
+
+function MockParser() {
+    this.counter = 0;
+    this.parse = function(value, rule){
         this.counter++;
         return Parser.parse(value, rule);
-    }
-}
+    };
+};
 
 describe('ParserMiddleware', function(){
 
     var cache           = new LRUCache(),
+
+        mockParser      = new MockParser(),
+        mockParser2     = new MockParser(),
+
         middleware      = new ParserMiddleware(cache, mockParser),
-        getHeaderFunc   = mockRequest.getHeader,
-        accessCounter   = 0;
+        middleware2     = new ParserMiddleware(cache, mockParser2),
+
+        getHeaderFunc   = mockRequest.getHeader;
 
     describe('#request', function(){
 
@@ -54,38 +98,88 @@ describe('ParserMiddleware', function(){
             });
 
             it('should parse filter headers', function(){
-                assert(mockRequest.getHeader('filter', true).length == 3);
-                assert(mockParser.counter===1);
+                assert.equal(3, mockRequest.getHeader('filter', true).length);
+                // counter is 3 because the headers are preprocessed now
+                assert.equal(3, mockParser.counter);
             });
 
             it('should cache the parsed filter header', function(){
                 assert(mockRequest.getHeader('filter', true).length == 3);
                 assert(cache.has(mockRequest._headers['filter']));
-                assert(mockParser.counter===1);
+                // counter is 3 because the headers are preprocessed now
+                assert.equal(mockParser.counter, 3);
             });
 
             it('should parse select headers', function(){
-                assert(mockRequest.getHeader('select', true).length == 3);
-                assert(mockParser.counter===2);
+                assert.equal(mockRequest.getHeader('select', true).length, 3);
+                assert.equal(mockParser.counter, 3);
             });
 
             it('should cache parsed select headers', function(){
-                assert(mockRequest.getHeader('select', true).length == 3);
+                assert.equal(mockRequest.getHeader('select', true).length, 3);
                 assert(cache.has(mockRequest._headers['select']));
-                assert(mockParser.counter===2);
+                assert.equal(mockParser.counter, 3);
             });
 
             it('should parse order headers', function(){
-                assert(mockRequest.getHeader('order', true).length == 2);
-                assert(mockParser.counter===3);
+                assert.equal(mockRequest.getHeader('order', true).length, 2);
+                assert.equal(mockParser.counter, 3);
             });
 
             it('should cache parsed order headers', function(){
-                assert(mockRequest.getHeader('order', true).length == 2);
+                assert.equal(mockRequest.getHeader('order', true).length, 2);
                 assert(cache.has(mockRequest._headers['order']));
-                assert(mockParser.counter===3);
+                assert.equal(mockParser.counter, 3);
             });
 
+        });
+    });
+    describe('#request (with syntax errors)', function(){
+
+        describe('on filter', function(){
+            middleware2.request(erroneousFilterMockRequest, {send: function(state, message){
+                it('should generate a 400 error if syntax errors occur', function(){
+                    assert.equal(state, 400);
+                });
+                it('should create an error message', function(){
+                    assert(!!message);
+                });
+            }}, function(){
+                it('should never call the passed next method on errors', function(){
+                    assert(false);
+                });
+            });
+        });
+
+        describe('on order', function(){
+            middleware2.request(erroneousOrderMockRequest, {send: function(state, message){
+                it('should generate a 400 error if syntax errors occur', function(){
+                    assert.equal(state, 400);
+                });
+                it('should create an error message', function(){
+                    assert(!!message);
+                });
+            }}, function(){
+                it('should never call the passed next method on errors', function(){
+                    assert(false);
+                });
+            });
+        });
+
+
+        describe('on select', function(){
+            middleware2.request(erroneousSelectMockRequest, {send: function(state, message){
+                it('should generate a 400 error if syntax errors occur', function(){
+                    assert.equal(state, 400);
+                });
+                it('should create an error message', function(){
+                    assert(!!message);
+                });
+            }}, function(){
+                it('should never call the passed next method on errors', function(){
+                    assert(false);
+                });
+            });
         });
     });
 });
